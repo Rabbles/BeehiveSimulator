5c371bc6279a8b8291be35868ce34866792a423d
Rab Horner <rab_horner@outlook.com>
2021-01-27 12:54:11 +0000

    Initial skeleton code for Flower

---
+++ BeehiveSimulator/Model/Flower.cs
@@ -0,0 +1,30 @@
+﻿using System.Drawing;
+
+namespace BeehiveSimulator.Model
+{
+    public class Flower
+    {
+        private int lifespan;
+
+        public const int LifeSpanMin = 15000;
+        public const int LifeSpanMax = 30000;
+        public const double InitialNectar = 1.5;
+
+        public Point Location { get; private set; }
+        public int Age { get; private set; }
+        public bool Alive { get; private set; }
+        public double Nectar { get; private set; }
+        public double NectarHarvested { get; set; }
+
+        public double HarvestNectar()
+        {
+            return 0.0;
+        }
+
+        public void Go()
+        {
+            
+        }
+
+    }
+}
